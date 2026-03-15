6ff46abab3d5eef37c7f7ed874fd38a4ec19bb5d
picadasonia <picadasonia@gmail.com>
2026-03-15 17:03:36 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+//Middlewares
+app.use(cors());
+app.use(express.json());
+
+//MySQL Connection
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'crud_api'
+});
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to MySQL:', err);
+        return;
+    }
+    console.log('Connected todatabase');
+});
+
+app.post('/api/users', (req, res) => {
+    const { name, email } = req.body;
+    const query = 'INSERT INTO users (name, email) VALUES (?, ?)';
+    db.query(query, [name, email], (err, result) => {
+        if (err) return res.status(500).json({ error: err.message });
+        res.status(201).json({ 
+            id: result.insertId, name, email 
+        });
+    });
+});
+
+app.get('/api/users', (req, res) => {
+    db.query('SELECT * FROM users ORDER BY id DESC', (err, results) => {
+        if (err) return res.status(500).json({ error: err.message });
+        res.json(results);
+    });
+});
+
+app.get('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    db.query('SELECT * FROM users WHERE id = ?', [id], (err, results) => {
+        if (err) return res.status(500).json({ error: err.message });
+        if (results.length === 0) return res.status(404).json({ error: 'User not found' });
+        res.json(results[0]);
+    });
+});
+
+app.put('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    const { name, email } = req.body;
+const sql = 'UPDATE users SET name = ?, email = ? WHERE id = ?';
+    db.query(sql, [name, email, id], (err, result) => {
+        if (err) return res.status(500).json({ error: err.message });
+        if (result.affectedRows === 0) return res.status(404).json({ error: 'User not found' });
+        res.json({ message: 'User updated successfully' });
+    });
+});
+
+app.delete('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    db.query('DELETE FROM users WHERE id = ?', [id], (err, result) => {
+        if (err) return res.status(500).json({ error: err.message });
+        if (result.affectedRows === 0) return res.status(404).json({ error: 'User not found' });
+        res.json({ message: 'User deleted successfully' });
+    });
+}); 
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost: ${port}`);
+});
